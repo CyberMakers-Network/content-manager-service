8aadb7d06662e88cf6bb6e1aa651b42b763cd2c6
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 20:03:14 -0300

    feat: add content data exception handler

--- src/main/kotlin/com/cyber/contentmanagerservice/domain/exceptions/ContentNotFoundException.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/domain/exceptions/ContentNotFoundException.kt
@@ -3,5 +3,5 @@ package com.cyber.contentmanagerservice.domain.exceptions
 import org.springframework.http.HttpStatus
 
 class ContentNotFoundException(
-    reason: String = "Content not found or reference id not registered."
+    reason: String = "Content not found or reference not registered."
 ) : ApiException(httpStatus = HttpStatus.NOT_FOUND, reason = reason)
