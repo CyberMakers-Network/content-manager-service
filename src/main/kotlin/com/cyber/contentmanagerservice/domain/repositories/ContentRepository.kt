51c220953fee2383f6ceb039261a7732ab180384
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 14:36:11 -0300

    fix: add repository annotation on interface

--- src/main/kotlin/com/cyber/contentmanagerservice/domain/repositories/ContentRepository.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/domain/repositories/ContentRepository.kt
@@ -3,5 +3,7 @@ package com.cyber.contentmanagerservice.domain.repositories
 import com.cyber.contentmanagerservice.domain.entities.Content
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ContentRepository : MongoRepository<Content, ObjectId>
