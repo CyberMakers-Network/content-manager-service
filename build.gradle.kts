9992321bc4813874f69c449988663014723f9689
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 00:14:55 -0300

    chore: add jacoco on project

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,59 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    jacoco
 }
 
 group = "com.cyber"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+}
+
+jacoco {
+    toolVersion = "0.8.7"
+    reportsDirectory.set(layout.buildDirectory.dir("jacocoReports"))
+}
+
+val excludePackages: Iterable<String> = listOf(
+    "**/com/cybermakers/contentmanagerservice/application/config/**",
+    "**/com/cybermakers/contentmanagerservice/domain/entities/**"
+)
+
+extra["excludePackages"] = excludePackages
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+
+    classDirectories.setFrom(
+        sourceSets.main.get().output.asFileTree.matching {
+            exclude(excludePackages)
+        }
+    )
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = 0.6.toBigDecimal()
+            }
+        }
+    }
+
+    classDirectories.setFrom(
+        sourceSets.main.get().output.asFileTree.matching {
+            exclude(excludePackages)
+        }
+    )
+
+    mustRunAfter(tasks["jacocoTestReport"])
 }
 
 dependencies {
