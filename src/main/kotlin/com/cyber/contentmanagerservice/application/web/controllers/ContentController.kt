dcabbc40e88fe5db612a658626783a43b8e768f7
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 20:02:30 -0300

    feat: add register content path

--- src/main/kotlin/com/cyber/contentmanagerservice/application/web/controllers/ContentController.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/application/web/controllers/ContentController.kt
@@ -1,10 +1,14 @@
 package com.cyber.contentmanagerservice.application.web.controllers
 
+import com.cyber.contentmanagerservice.application.payloads.request.ContentRequest
 import com.cyber.contentmanagerservice.application.payloads.response.ContentResponse
 import com.cyber.contentmanagerservice.domain.services.ContentService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,6 +17,13 @@ import org.springframework.web.bind.annotation.RestController
 class ContentController(
     private val contentService: ContentService
 ) {
+
+    @PostMapping
+    fun register(@RequestBody newContent: ContentRequest): ResponseEntity<ContentResponse> {
+        return contentService.register(newContent).let {
+            ResponseEntity(ContentResponse(it), HttpStatus.CREATED)
+        }
+    }
 
     @GetMapping("/{title}")
     fun getContentByTitle(@PathVariable title: String): ResponseEntity<ContentResponse> {
