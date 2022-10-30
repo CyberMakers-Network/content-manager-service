306fd1091cc221d394ea6f924f566e25554f902f
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 20:02:49 -0300

    fix: content data value

--- src/main/kotlin/com/cyber/contentmanagerservice/domain/entities/Content.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/domain/entities/Content.kt
@@ -1,20 +1,25 @@
 package com.cyber.contentmanagerservice.domain.entities
 
-import org.bson.types.ObjectId
-import org.springframework.data.annotation.Id
+import com.cyber.contentmanagerservice.application.payloads.request.ContentRequest
 import java.time.LocalDateTime
 
 data class Content(
-    @Id
-    val _id: ObjectId,
-
     val title: String,
     val subtitle: String?,
     val description: String,
     val datetime: LocalDateTime? = LocalDateTime.now(),
     val url: String,
     val type: Type
-)
+) {
+    constructor(newContent: ContentRequest) : this(
+        title = newContent.title,
+        subtitle = newContent.subtitle,
+        description = newContent.description,
+        datetime = newContent.datetime,
+        url = newContent.url,
+        type = newContent.type
+    )
+}
 
 enum class Type {
     REPOSITORY, ARTICLE
