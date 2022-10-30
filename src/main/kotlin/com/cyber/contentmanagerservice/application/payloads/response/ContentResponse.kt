6c6968ff0609357a7f86cbd188d54142103bd350
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 17:03:36 -0300

    feat: add content response class

---
+++ src/main/kotlin/com/cyber/contentmanagerservice/application/payloads/response/ContentResponse.kt
@@ -0,0 +1,23 @@
+package com.cyber.contentmanagerservice.application.payloads.response
+
+import com.cyber.contentmanagerservice.domain.entities.Content
+import com.cyber.contentmanagerservice.domain.entities.Type
+import java.time.LocalDateTime
+
+data class ContentResponse(
+    val title: String,
+    val subtitle: String?,
+    val description: String,
+    val dateTime: LocalDateTime?,
+    val url: String,
+    val type: Type
+) {
+    constructor(content: Content) : this(
+        title = content.title,
+        subtitle = content.subtitle,
+        description = content.description,
+        dateTime = content.datetime,
+        url = content.url,
+        type = content.type
+    )
+}
