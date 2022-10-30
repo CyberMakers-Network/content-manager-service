ca1b73ee01da63d3da1cb355c6eba789d4b98b8c
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 17:03:09 -0300

    fix: fix method signature

--- src/main/kotlin/com/cyber/contentmanagerservice/domain/services/ContentService.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/domain/services/ContentService.kt
@@ -1,8 +1,7 @@
 package com.cyber.contentmanagerservice.domain.services
 
 import com.cyber.contentmanagerservice.domain.entities.Content
-import org.bson.types.ObjectId
 
 interface ContentService {
-    fun getById(id: ObjectId): Content?
+    fun findByTitle(title: String): Content?
 }
--- src/main/kotlin/com/cyber/contentmanagerservice/domain/services/impl/ContentServiceImpl.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/domain/services/impl/ContentServiceImpl.kt
@@ -4,7 +4,6 @@ import com.cyber.contentmanagerservice.domain.entities.Content
 import com.cyber.contentmanagerservice.domain.exceptions.ContentNotFoundException
 import com.cyber.contentmanagerservice.domain.repositories.ContentRepository
 import com.cyber.contentmanagerservice.domain.services.ContentService
-import org.bson.types.ObjectId
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -14,9 +13,9 @@ class ContentServiceImpl(
     private val contentRepository: ContentRepository
 ) : ContentService {
 
-    override fun getById(id: ObjectId): Content? {
-        logger.info("Finding content with id=$id")
-        return contentRepository.findById(id).orElseThrow { ContentNotFoundException() }
+    override fun findByTitle(title: String): Content? {
+        logger.info("Finding content with title=$title")
+        return contentRepository.findByTitle(title).orElseThrow { ContentNotFoundException() }
     }
 
     companion object {
