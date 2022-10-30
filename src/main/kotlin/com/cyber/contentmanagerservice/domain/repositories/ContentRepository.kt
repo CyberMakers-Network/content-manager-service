f8071b5a812be258cf3af4051305afea158e7b0a
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-30 17:01:54 -0300

    fix: fix repository id

--- src/main/kotlin/com/cyber/contentmanagerservice/domain/repositories/ContentRepository.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/domain/repositories/ContentRepository.kt
@@ -1,9 +1,12 @@
 package com.cyber.contentmanagerservice.domain.repositories
 
 import com.cyber.contentmanagerservice.domain.entities.Content
-import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
+import java.util.UUID
 
 @Repository
-interface ContentRepository : MongoRepository<Content, ObjectId>
+interface ContentRepository : MongoRepository<Content, UUID> {
+    fun findByTitle(title: String): Optional<Content>
+}
