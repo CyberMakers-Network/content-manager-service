704c447b76bfd3bb672d92629b61598cd42c65da
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-11-02 15:57:21 -0300

    fix: add _id reference

--- src/main/kotlin/com/cyber/contentmanagerservice/domain/entities/Author.kt
+++ src/main/kotlin/com/cyber/contentmanagerservice/domain/entities/Author.kt
@@ -1,6 +1,9 @@
 package com.cyber.contentmanagerservice.domain.entities
 
+import org.bson.types.ObjectId
+
 data class Author(
+    val _id: ObjectId? = null,
     val name: String,
     val surname: String,
     val email: String,
