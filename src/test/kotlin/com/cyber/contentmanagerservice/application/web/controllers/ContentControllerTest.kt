3beea1e441918c65c2ea953527a449cf24c88985
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-11-03 19:48:17 -0300

    test: create a new test to create controller

--- src/test/kotlin/com/cyber/contentmanagerservice/application/web/controllers/ContentControllerTest.kt
+++ src/test/kotlin/com/cyber/contentmanagerservice/application/web/controllers/ContentControllerTest.kt
@@ -1,9 +1,17 @@
 package com.cyber.contentmanagerservice.application.web.controllers
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.cyber.contentmanagerservice.domain.factories.ContentFactory
+import com.cyber.contentmanagerservice.domain.factories.ContentRequestFactory
 import com.cyber.contentmanagerservice.domain.services.ContentService
 import io.mockk.clearAllMocks
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
 
 class ContentControllerTest {
 
@@ -16,5 +24,31 @@ class ContentControllerTest {
     @BeforeEach
     fun init() {
         clearAllMocks()
+    }
+
+    @Test
+    fun `it should be register a new content`() {
+        val mockContentRequest = ContentRequestFactory.sample()
+        val mockContent = ContentFactory.sample()
+
+        every {
+            contentService.register(any())
+        } returns mockContent
+
+        val result = controller.register(mockContentRequest)
+
+        verify(exactly = 1) {
+            contentService.register(any())
+        }
+
+        assertThat(result.statusCode).isEqualTo(HttpStatus.CREATED)
+
+        assertThat(result.body!!.title).isEqualTo(mockContent.title)
+        assertThat(result.body!!.subtitle).isEqualTo(mockContent.subtitle)
+        assertThat(result.body!!.description).isEqualTo(mockContent.description)
+        assertThat(result.body!!.dateTime).isEqualTo(mockContent.datetime)
+        assertThat(result.body!!.authorEmail).isEqualTo(mockContent.authorEmail)
+        assertThat(result.body!!.type).isEqualTo(mockContent.type)
+        assertThat(result.body!!.url).isEqualTo(mockContent.url)
     }
 }
